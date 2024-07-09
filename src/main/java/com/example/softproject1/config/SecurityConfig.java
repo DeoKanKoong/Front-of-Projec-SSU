e2fc64448c4562e45c2f0f11af81481c5b9ed89a
BBIYACK-0834 <erty0834@naver.com>
2024-07-10 04:00:35 +0900

    articles 로그인 없이 확인할 수 있도록 수정

--- src/main/java/com/example/softproject1/config/SecurityConfig.java
+++ src/main/java/com/example/softproject1/config/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/", "/login", "loginProc", "/join", "joinProc","/logout").permitAll()
+                        .requestMatchers("/", "/login", "loginProc", "/join", "joinProc","/logout","/articles").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
                         .anyRequest().authenticated()

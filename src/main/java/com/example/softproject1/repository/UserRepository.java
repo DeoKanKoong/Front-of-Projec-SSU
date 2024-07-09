bf2c3fa0967055723610cefee577691f151997dd
BBIYACK-0834 <erty0834@naver.com>
2024-07-10 03:12:53 +0900

    로그인 기본 구현에 게시판 구동 확인까지 완료

---
+++ src/main/java/com/example/softproject1/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.softproject1.repository;
+
+import com.example.softproject1.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+    boolean existsByUsername(String username);
+
+
+    UserEntity findByUsername(String username);
+}

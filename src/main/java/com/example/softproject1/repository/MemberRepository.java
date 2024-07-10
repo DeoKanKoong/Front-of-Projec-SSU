aee4b1fd9c2b636b8fe6a38dbb8b0c751131d16a
Seonjeongg <132347712+Seonjeongg@users.noreply.github.com>
2024-07-10 23:13:06 +0900

    Delete src/main/java/com/example/softproject1/repository/MemberRepository.java

--- src/main/java/com/example/softproject1/repository/MemberRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.softproject1.repository;
-import com.example.softproject1.entity.Member;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MemberRepository extends CrudRepository<Member, Long> {
-}
